ec5673524fc772b0ce5fc156dea18cd75efb1668
Nicolas <nicolas@archivados.com>
2026-01-22 17:39:24 +0100

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@
 
 import { useTranslations } from 'next-intl';
 import { motion } from 'framer-motion';
-import { Link } from '@/i18n/routing';
+import { Link } from '@/i18n/navigation';
 
 export default function Hero() {
     const t = useTranslations('HomePage');
